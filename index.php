cbcf7bc66cfc9d79e684f2ee4ab8f6d2e11ef120
System Administrator <root@Pauls-MBP.default>
2017-11-12 00:46:26 +0000

    {I am creating a portfolio website and I just created the navigation bar for the website}

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Paul Didi - Portfolio</title>
+		<meta charset="utf-8">
+		 <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-default ">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" 
+						data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">PAUL DIDI</a>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Home</a></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle"  data-toggle="dropdown" href="#">Portfolio<span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Men's Collection</a></li>
+								<li><a href="#">Women's Collection</a></li>
+								<li><a href="#">Kid's Collection</a></li>
+							</ul>
+					</li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</body>
+</html>
